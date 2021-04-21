0d214986a6002a43dc372e83dcb8a284338fee71
Raul Ramos <cagefightingstfc@yahoo.com>
2021-04-20 20:45:08 -0500

    chitchat

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div
-    class="min-h-screen flex justify-center items-center text-coolGray-300 font-extralight space-x-4 text-6xl"
-  >
-    <mdi:controller-classic-outline />
-    <h1 class="">Hello There</h1>
-  </div>
+  <NavBar />
+  <Messages v-if="isAuthenticated" />
+  <NoAuth v-else />
 </template>
+
+<script setup>
+import { authentication } from "~/helpers/useFirebase";
+const { isAuthenticated } = authentication();
+</script>
